8b01b64290d002a703bb5e0186f40f18d6f97ab7
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-07-19 08:06:44 -0500

    Updates to neovim configuration

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -1,18 +1,22 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          -- '.git',
-          -- '.DS_Store',
-          -- 'thumbs.db',
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = false, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
+          always_show = { -- remains visible even if other settings would normally hide it
+            ".gitignore",
+          },
+          never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+            ".DS_Store",
+            "thumbs.db",
+            ".git",
+          },
         },
-        never_show = {},
       },
     },
   },
