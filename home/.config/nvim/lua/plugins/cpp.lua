51e932349d425c39708ba906a5ef456453c7d1de
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-03-16 16:45:32 -0500

    neovim: Improve clangd root detection configuration

--- home/.config/nvim/lua/plugins/cpp.lua
+++ home/.config/nvim/lua/plugins/cpp.lua
@@ -1,10 +1,22 @@
+local lspconfig = require("lspconfig")
+
 return {
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
       servers = {
-        clangd = {},
+        clangd = {
+          root_dir = lspconfig.util.root_pattern(
+            "compile_commands.json",
+            ".git",
+            ".clangd",
+            ".clang-tidy",
+            ".clang-format",
+            "compile_flags.txt",
+            "configure.ac"
+          ),
+        },
       },
     },
   },
