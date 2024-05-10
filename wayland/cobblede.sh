10481a7e78063c1ee09b3867e3bd5c204ffa07be
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-05-10 18:18:39 -0500

    cobblede: Set xcursor theme/size
    In a recent update, Hyprland began using hyprcursor for cursor theming.
    This system is incompatible with xcursors, and these environment
    variables must be set to configure Hyprland server-side cursors when no
    hyprcursor themes are available.

--- wayland/cobblede.sh
+++ wayland/cobblede.sh
@@ -5,3 +5,5 @@ export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export XDG_SESSION_DESKTOP=hyprland
 export XDG_CURRENT_DESKTOP=hyprland
+export XCURSOR_THEME=Adwaita
+export XCURSOR_SIZE=24
