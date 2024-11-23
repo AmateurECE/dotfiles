c9a3ea3f9311de83dfc0410f3c52350b634e4117
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-11-23 08:57:52 -0600

    cobblede: Remove XDG_{CURRENT,SESSION}_DESKTOP vars
    Apparently, Hyprland now sets these variables in the shell, and warns if
    they have been set externally.

--- wayland/cobblede.sh
+++ wayland/cobblede.sh
@@ -3,7 +3,5 @@ export MOZ_ENABLE_WAYLAND=1
 export QT_QPA_PLATFORM=wayland
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
-export XDG_SESSION_DESKTOP=hyprland
-export XDG_CURRENT_DESKTOP=hyprland
 export XCURSOR_THEME=Adwaita
 export XCURSOR_SIZE=24
